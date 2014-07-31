464deff3467a4fec610329bc35af386147f2a6a0
prateekchandan <prateekchandan5545@gmail.com>
2014-07-31 22:01:20 +0530

    added new tempalte

---
+++ app/views/team/2014.blade.php
@@ -0,0 +1,188 @@
+@extends('layout.default')
+
+@section('content')
+<section id="team" class="light-bg">
+				<div class="container inner-top inner-bottom-sm">
+					
+					<div class="row">
+						<div class="col-md-8 col-sm-10 center-block text-center animated fadeInUp" style="opacity: 0;">
+							<header>
+								<h1>The people behind the magic</h1>
+								<p>Magnis modipsae que voloratati andigen daepeditem quiate re porem aut labor.</p>
+							</header>
+						</div><!-- /.col -->
+					</div><!-- /.row -->
+					
+					<div class="row inner-top-sm text-center">
+						
+						<div class="col-sm-4 inner-bottom-sm inner-left inner-right animated fadeInUp" style="opacity: 0;">
+							<figure class="member">
+								
+								<div class="icon-overlay icn-link">
+									<a href="#"><span class="icn-more"></span><img src="assets/images/art/human03.jpg" class="img-circle"></a>
+								</div><!-- /.icon-overlay -->
+								
+								<figcaption>
+								
+									<h2>
+										Chris Cavill
+										<span>Creative Director</span>
+									</h2>
+									
+									<blockquote>Magnis modipsae que voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes es modis.</blockquote>
+									
+									<ul class="social">
+										<li><a href="#"><i class="icon-s-facebook"></i></a></li>
+										<li><a href="#"><i class="icon-s-gplus"></i></a></li>
+										<li><a href="#"><i class="icon-s-twitter"></i></a></li>
+									</ul><!-- /.social -->
+									
+								</figcaption>
+								
+							</figure>
+						</div><!-- /.col -->
+						
+						<div class="col-sm-4 inner-bottom-sm inner-left inner-right animated fadeInUp" style="opacity: 0;">
+							<figure class="member">
+								
+								<div class="icon-overlay icn-link">
+									<a href="#"><span class="icn-more"></span><img src="assets/images/art/human01.jpg" class="img-circle"></a>
+								</div><!-- /.icon-overlay -->
+								
+								<figcaption>
+								
+									<h2>
+										Amber Jones
+										<span>Marketing Director</span>
+									</h2>
+									
+									<blockquote>Magnis modipsae que voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes es modis.</blockquote>
+									
+									<ul class="social">
+										<li><a href="#"><i class="icon-s-facebook"></i></a></li>
+										<li><a href="#"><i class="icon-s-gplus"></i></a></li>
+										<li><a href="#"><i class="icon-s-twitter"></i></a></li>
+									</ul><!-- /.social -->
+									
+								</figcaption>
+								
+							</figure>
+						</div><!-- /.col -->
+						
+						<div class="col-sm-4 inner-bottom-sm inner-left inner-right animated fadeInUp" style="opacity: 0;">
+							<figure class="member">
+								
+								<div class="icon-overlay icn-link">
+									<a href="#"><span class="icn-more"></span><img src="assets/images/art/human05.jpg" class="img-circle"></a>
+								</div><!-- /.icon-overlay -->
+								
+								<figcaption>
+								
+									<h2>
+										Sophie Adams
+										<span>Technical Director</span>
+									</h2>
+									
+									<blockquote>Magnis modipsae que voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes es modis.</blockquote>
+									
+									<ul class="social">
+										<li><a href="#"><i class="icon-s-facebook"></i></a></li>
+										<li><a href="#"><i class="icon-s-gplus"></i></a></li>
+										<li><a href="#"><i class="icon-s-twitter"></i></a></li>
+									</ul><!-- /.social -->
+									
+								</figcaption>
+								
+							</figure>
+						</div><!-- /.col -->
+						
+					</div><!-- /.row -->
+					
+					<div class="row text-center">
+						
+						<div class="col-sm-4 inner-bottom-sm inner-left inner-right animated fadeInUp" style="opacity: 0;">
+							<figure class="member">
+								
+								<div class="icon-overlay icn-link">
+									<a href="#"><span class="icn-more"></span><img src="assets/images/art/human06.jpg" class="img-circle"></a>
+								</div><!-- /.icon-overlay -->
+								
+								<figcaption>
+								
+									<h2>
+										Amy Williams 
+										<span>Senior Designer</span>
+									</h2>
+									
+									<blockquote>Magnis modipsae que voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes es modis.</blockquote>
+									
+									<ul class="social">
+										<li><a href="#"><i class="icon-s-facebook"></i></a></li>
+										<li><a href="#"><i class="icon-s-gplus"></i></a></li>
+										<li><a href="#"><i class="icon-s-twitter"></i></a></li>
+									</ul><!-- /.social -->
+									
+								</figcaption>
+								
+							</figure>
+						</div><!-- /.col -->
+						
+						<div class="col-sm-4 inner-bottom-sm inner-left inner-right animated fadeInUp" style="opacity: 0;">
+							<figure class="member">
+								
+								<div class="icon-overlay icn-link">
+									<a href="#"><span class="icn-more"></span><img src="assets/images/art/human04.jpg" class="img-circle"></a>
+								</div><!-- /.icon-overlay -->
+								
+								<figcaption>
+								
+									<h2>
+										Toby Evans 
+										<span>Designer</span>
+									</h2>
+									
+									<blockquote>Magnis modipsae que voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes es modis.</blockquote>
+									
+									<ul class="social">
+										<li><a href="#"><i class="icon-s-facebook"></i></a></li>
+										<li><a href="#"><i class="icon-s-gplus"></i></a></li>
+										<li><a href="#"><i class="icon-s-twitter"></i></a></li>
+									</ul><!-- /.social -->
+									
+								</figcaption>
+								
+							</figure>
+						</div><!-- /.col -->
+						
+						<div class="col-sm-4 inner-bottom-sm inner-left inner-right animated fadeInUp" style="opacity: 0;">
+							<figure class="member">
+								
+								<div class="icon-overlay icn-link">
+									<a href="#"><span class="icn-more"></span><img src="assets/images/art/human02.jpg" class="img-circle"></a>
+								</div><!-- /.icon-overlay -->
+								
+								<figcaption>
+								
+									<h2>
+										Jennifer Fox
+										<span>Project Manager</span>
+									</h2>
+									
+									<blockquote>Magnis modipsae que voloratati andigen daepeditem quiate re porem aut labor. Laceaque quiae sitiorem rest non restibusaes es modis.</blockquote>
+									
+									<ul class="social">
+										<li><a href="#"><i class="icon-s-facebook"></i></a></li>
+										<li><a href="#"><i class="icon-s-gplus"></i></a></li>
+										<li><a href="#"><i class="icon-s-twitter"></i></a></li>
+									</ul><!-- /.social -->
+									
+								</figcaption>
+								
+							</figure>
+						</div><!-- /.col -->
+						
+					</div><!-- /.row -->
+					
+				</div><!-- /.container -->
+			</section>
+@endsection
