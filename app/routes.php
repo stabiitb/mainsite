8860e5ba464a750375efa07c06e2579fca18cf5b
prateekchandan <prateekchandan5545@gmail.com>
2014-08-03 16:47:50 +0530

    Updated robotics Club

--- app/routes.php
+++ app/routes.php
@@ -7,9 +7,14 @@ Route::get('aeromodelling-club',array('as'=>'aeromodelling-club','uses'=>'HomeCo
 Route::get('electronics-club',array('as'=>'electronics-club','uses'=>'HomeController@electronics_club'));
 Route::get('krittika',array('as'=>'krittika','uses'=>'HomeController@krittika'));
 Route::get('math-and-physics-club',array('as'=>'MnP','uses'=>'HomeController@MnP'));
-Route::get('robotics-club',array('as'=>'robotics-club','uses'=>'HomeController@robotics-club'));
+Route::get('robotics-club',array('as'=>'robotics-club','uses'=>'HomeController@robotics_club'));
 Route::get('web-and-coding-club',array('as'=>'wncc','uses'=>'HomeController@wncc'));
 Route::get('technovation',array('as'=>'technovation','uses'=>'HomeController@technovation'));
+
+
+
+// Clubs Pages
+Route::get('robotics-club/{page}',array('as'=>'robotics-team','uses'=>'HomeController@robotics_club'));
 
 // Vision
 Route::get('vision-of-stab-team-2014-15',array('as'=>'vision','uses'=>'HomeController@vision'));
--- app/views/club/elec.blade.php
+++ app/views/club/elec.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
 yo !
+
 @endsection
