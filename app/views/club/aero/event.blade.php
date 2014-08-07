8190be109cf9ce4cc0a3ec4e41309119ae866958
prateekchandan <prateekchandan5545@gmail.com>
2014-08-07 22:38:44 +0530

    added event on aero

--- app/views/club/aero/event.blade.php
+++ app/views/club/aero/event.blade.php
@@ -2,8 +2,48 @@
 
 @section('inner-content')
 <!-- ============================================================= SECTION – BLOG ============================================================= -->
-			<section>
-				<h2>Coming soon..</h2>
+			<section>		
+							<div class="posts sidemeta">
+								
+							
+								<div class="post format-standard">
+									
+									<div class="date-wrapper">
+										<div class="date">
+											<span class="day">7</span>
+											<span class="month">August</span>
+										</div><!-- /.date -->
+									</div><!-- /.date-wrapper -->
+									
+									<div class="format-wrapper">
+										<a href="#" data-filter=".format-standard"><i class="icon-edit"></i></a>
+									</div><!-- /.format-wrapper -->
+									
+									<div class="post-content">
+										
+										<h2 class="post-title">
+											<a href="blog-post.html"><b>"Missile"</b> - Water rocket workshop and competition</a>
+										</h2>
+										
+										
+										<p>Hello everyone,<br>
+										I am pleased to announce the results of yesterday's(6 Aug,2014) competition held after the Aeromodelling Club event "Missile". We were happy to see such a great enthusiasm in you, and we're sure that you'll continue to have a lot of fun in the upcoming events. Stay tuned.
+										</p>
+										<p>
+										 the winning teams are -<br>
+											First place - Team-<b>COSMOS (Ila Toppo)</b> Best time- 5.64 Sec<br>
+											Second place - Team -<b> UDDAN (Siddrath Keshan)</b> - 5.51 Sec
+										</p>
+										<p>
+										</p>
+										
+									</div><!-- /.post-content --> 
+									
+								</div><!-- /.post -->
+								
+								
+								
+							</div><!-- /.posts -->
 			</section>
 			
 			
