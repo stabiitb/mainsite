bdb01f5374d109e2759cfd047527916a4b77ca01
prateekchandan <prateekchandan5545@gmail.com>
2014-09-06 16:44:07 +0530

    updated mnp club

--- app/views/club/mnp/event.blade.php
+++ app/views/club/mnp/event.blade.php
@@ -3,7 +3,22 @@
 @section('inner-content')
 <!-- ============================================================= SECTION – BLOG ============================================================= -->
 			<section>
-				<h2>Coming soon..</h2>
+				<h2>List of events throughout the Year</h2>
+				<ul class="arrowed">
+					<li>Experimental demonstration</li>
+					<li>Maths Olympic and Mathematics question solving sessions</li>
+					<li>In-sem Projects</li>
+					<li>LogicGC</li>
+					<li>Quiz GC</li>
+					<li>Lectures/talks</li>
+					<li>GD/ Club open meetings</li>
+					<li>Lab Visit</li>
+					<li>Documentary/Movie Screening</li>
+					<li>Puzzles solving sessions</li>
+					<li>Exclusive Physics and Mathematics Quiz</li>
+					<li>Gaming Sessions (logical games, boardgames)</li>
+					<li>Outstation Research Laboratory Trip</li>
+				</ul>
 			</section>
 			
 			
