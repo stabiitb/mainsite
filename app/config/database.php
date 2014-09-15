0346132d3d584755e91760d0d4eead19885768c5
prateekchandan <prateekchandan5545@gmail.com>
2014-09-15 12:23:25 +0000

    server update

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => '',
-			'password'  => '',
+			'database'  => 'stabiitb',
+			'username'  => 'stab_manager',
+			'password'  => '<Stabiitb7@>',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
