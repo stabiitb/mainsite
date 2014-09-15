d9a551435d2231660726605aa4177dacb05d93bd
prateekchandan <prateekchandan5545@gmail.com>
2014-09-16 00:14:32 +0530

    finished unfinished task

---
+++ app/views/home/facad.blade.php
@@ -0,0 +1,118 @@
+@extends('layout.default')
+
+@section('content')
+<main>
+			
+			
+			<section class="light-bg img-bg img-bg-softer">
+				<div class="container" style="padding-top:50px;padding-bottom:50px;">
+					<div class="row">
+						<div class="col-md-8">
+							<header>
+								<h2>Faculty Advisors and Mentors</h2>
+							</header>
+						</div><!-- /.col -->
+					</div><!-- ./row -->
+				</div><!-- /.container -->
+			</section>
+			
+			<!-- ============================================================= SECTION – HERO : END ============================================================= -->
+			
+			<div class="container inner">
+				<div class="row">
+				
+					<div class="col-md-12 inner-right-md">
+						
+						<!-- ============================================================= SECTION – CONTACT FORM ============================================================= -->
+						
+						<section id="contact-form">
+							
+							
+														<style>
+							    .manager-img{ width:130px; height:130px; border-radius:5px; box-shadow:0px
+							    0px 8px #ccc; } .prof-name{ font-weight:bold; }
+							</style>
+							<table border="0" align="center" class="table">
+							    <tbody>
+							        <tr>
+							            <td>
+							                <a class="prof-name" href="http://www.aero.iitb.ac.in/~arya/">Prof Hemendra Arya </a>
+							            </td>
+							            <td>
+							                Current Facad
+							            </td>
+							            <td>
+							                <img src="http://www.aero.iitb.ac.in/~arya/arya.jpg" class="manager-img">
+							            </td>
+							        </tr>
+							        <tr>
+							            <td>
+							                <a class="prof-name" href="https://www.ee.iitb.ac.in/~hn/">Prof H naraynan</a>
+							            </td>
+							            <td>
+							                Former Facad
+							            </td>
+							            <td>
+							                <img src="https://www.ee.iitb.ac.in/~hn/Figs/hn.jpg" class="manager-img">
+							            </td>
+							        </tr>
+							        <tr>
+							            <td>
+							                <a class="prof-name" href="https://www.ee.iitb.ac.in/wiki/faculty/vasi">Prof J Vasi</a>
+							            </td>
+							            <td>
+							                Faculty Mentor
+							            </td>
+							            <td>
+							                <img src="https://www.ee.iitb.ac.in/wiki/_media/faculty/vasi.jpg?w=130&h=&cache=cache"
+							                class="manager-img">
+							            </td>
+							        </tr>
+							        <tr>
+							            <td>
+							                <a class="prof-name" href="https://www.ee.iitb.ac.in/wiki/faculty/mbpatil">Prof M.B. Patil</a>
+							            </td>
+							            <td>
+							                Faculty Mentor
+							            </td>
+							            <td>
+							                <img src="https://www.ee.iitb.ac.in/wiki/_media/faculty/prof.mbpatil.jpg?w=130&h=&cache=cache"
+							                class="manager-img">
+							            </td>
+							        </tr>
+							        <tr>
+							            <td>
+							                <a class="prof-name" href="https://www.ee.iitb.ac.in/wiki/faculty/jjohn"> Prof J John</a>
+							            </td>
+							            <td>
+							                Tinkerers' Lab faculty champions
+							            </td>
+							            <td>
+							                <img src="https://www.ee.iitb.ac.in/wiki/_media/faculty/jjohn.jpg?w=130&h=&cache=cache"
+							                class="manager-img">
+							            </td>
+							        </tr>
+							        <tr>
+							            <td>
+							                <a class="prof-name" href="http://www.me.iitb.ac.in/~ramesh/"> Prof Ramesh Singh </a>
+							            </td>
+							            <td>
+							                Tinkerers' Lab faculty champions
+							            </td>
+							            <td>
+							                <img src="http://www.me.iitb.ac.in/~ramesh/home_files/image3381.jpg" class="manager-img">
+							            </td>
+							        </tr>
+							</table>
+														
+						</section>
+						
+					
+					</div><!-- ./col -->
+					
+						
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+			
+</main>
+@endsection
---
+++ app/views/team/2013.blade.php
@@ -0,0 +1,316 @@
+@extends('layout.default')
+
+@section('content')
+<main>
+			
+			
+			<section class="light-bg img-bg img-bg-softer">
+				<div class="container" style="padding-top:50px;padding-bottom:50px;">
+					<div class="row">
+						<div class="col-md-8">
+							<header>
+								<h2>Stab Team 2013</h2>
+							</header>
+						</div><!-- /.col -->
+					</div><!-- ./row -->
+				</div><!-- /.container -->
+			</section>
+			
+			<!-- ============================================================= SECTION – HERO : END ============================================================= -->
+			
+			<div class="container inner">
+				<div class="row">
+				
+					<div class="col-md-12 inner-right-md">
+						
+						<!-- ============================================================= SECTION – CONTACT FORM ============================================================= -->
+						
+						<section id="contact-form">
+							
+							<table border="0" align="center" class="table">
+						    <tbody>
+						        <tr>
+						            <td colspan="3">
+						                <h2>
+						                    Overall Co - ordinator:
+						                </h2>
+						            </td>
+						        </tr>
+						        <tr>
+						            <td>
+						                <h2>
+						                    <span style="font-size: 14pt;">
+						                        Ashima Mittal
+						                    </span>
+						                </h2>
+						            </td>
+						            <td>
+						                <h2>
+						                    <span style="font-size: 12pt;">
+						                        <a href="mailto:ashima.mittal@gmail.com">
+						                                                ashima.mittal@gmail.com
+						                                                </a>
+						                    </span>
+						                </h2>
+						            </td>
+						            <td>
+						                <h2>
+						                    <span style="font-size: 12pt;">
+						                        9167146655
+						                    </span>
+						                </h2>
+						            </td>
+						        </tr>
+						        <tr>
+						            <td colspan="3">
+						                <h2>
+						                    Club Managers:
+						                </h2>
+						            </td>
+						        </tr>
+						        <tr>
+						            <td colspan="3">
+						                <span style="font-size: 12pt;">
+						                    <b>
+						                        Aeromodelling Club:
+						                    </b>
+						                </span>
+						            </td>
+						        </tr>
+						        <tr>
+						            <td>
+						                Varun S S
+						            </td>
+						            <td>
+						                <a href="mailto:varunss1993@gmail.com">varunss1993@gmail.com</a>
+						            </td>
+						            <td>
+						                9920136821
+						            </td>
+						        </tr>
+						        <tr>
+						            <td colspan="3">
+						                <span style="font-size: 12pt;">
+						                    <b>
+						                        Electronics Club:
+						                    </b>
+						                </span>
+						            </td>
+						        </tr>
+						        <tr>
+						            <td>
+						                Lalit Kumar
+						            </td>
+						            <td>
+						                <a href="mailto:lalit.lalit06110@gmail.com">lalit.lalit06110@gmail.com</a>
+						            </td>
+						            <td>
+						                9833371495
+						            </td>
+						        </tr>
+						        <tr>
+						            <td>
+						                Jayesh Hannurkar
+						            </td>
+						            <td>
+						                <a dir="ltr" href="mailto:jayeshhannurkar@gmail.com" target="_blank" rel="nofollow"> jayeshhannurkar@gmail.com </a>
+						            </td>
+						            <td>
+						                9769694822
+						            </td>
+						        </tr>
+						        <tr>
+						            <td colspan="3">
+						                <span style="font-size: 12pt;">
+						                    <b>
+						                        Krittika:
+						                    </b>
+						                </span>
+						            </td>
+						        </tr>
+						        <tr>
+						            <td>
+						                Ruturaj Atre
+						            </td>
+						            <td>
+						                <a href="mailto:ruturaj.atre@gmail.com">ruturaj.atre@gmail.com</a>
+						            </td>
+						            <td>
+						                9922902964
+						            </td>
+						        </tr>
+						        <tr>
+						            <td colspan="3">
+						                <span style="font-size: 12pt;">
+						                    <b>
+						                        Maths &amp; Physics Club:
+						                    </b>
+						                </span>
+						            </td>
+						        </tr>
+						        <tr>
+						            <td>
+						                Rahul Kumar
+						            </td>
+						            <td>
+						                <a href="mailto:krrahul011@gmail.com"> krrahul011@gmail.com </a>
+						            </td>
+						            <td>
+						                9167782254
+						            </td>
+						        </tr>
+						        <tr>
+						            <td colspan="3">
+						                <span style="font-size: 12pt;">
+						                    <b>
+						                        Robotics Club:
+						                    </b>
+						                </span>
+						            </td>
+						        </tr>
+						        <tr>
+						            <td>
+						                Krishnakant Saboo
+						            </td>
+						            <td>
+						                <a href="mailto:kishansaboo.2004@gmail.com">kishansaboo.2004@gmail.com</a>
+						            </td>
+						            <td>
+						                9820715401
+						            </td>
+						        </tr>
+						        <tr>
+						            <td>
+						                Himani Singhal
+						            </td>
+						            <td>
+						                <a href="mailto:himanisinghal92@gmail.com"> himanisinghal92@gmail.com </a>
+						            </td>
+						            <td>
+						                9405621818
+						            </td>
+						        </tr>
+						        <tr>
+						            <td colspan="3">
+						                <span style="font-size: 12pt;">
+						                    <b>
+						                        Web and Coding Club:
+						                    </b>
+						                </span>
+						            </td>
+						        </tr>
+						        <tr>
+						            <td>
+						                Sushant Hiray
+						            </td>
+						            <td>
+						                hiraysushant@gmail.com
+						            </td>
+						            <td>
+						                7588557512
+						            </td>
+						        </tr>
+						        <tr>
+						            <td>
+						                Guna Prasaad
+						            </td>
+						            <td>
+						                <a href="mailto:gunaprsd@gmail.com">gunaprsd@gmail.com</a>
+						            </td>
+						            <td>
+						                9920147551
+						            </td>
+						        </tr>
+						        <tr>
+						            <td colspan="3">
+						                <h3>
+						                    <strong>
+						                        <span style="font-size: 12pt;">
+						                            Technovation:
+						                        </span>
+						                    </strong>
+						                </h3>
+						            </td>
+						        </tr>
+						        <tr>
+						            <td>
+						                Kunal Phalak
+						            </td>
+						            <td>
+						                <a href="mailto:kunalphalakiit@gmail.com">kunalphalakiit@gmail.com</a>
+						            </td>
+						            <td>
+						                9167782655
+						            </td>
+						        </tr>
+						        <tr>
+						            <td colspan="3">
+						                <h3>
+						                    <span style="font-size: 12pt;">
+						                        <strong>
+						                            Web Manager:
+						                        </strong>
+						                    </span>
+						                </h3>
+						            </td>
+						        </tr>
+						        <tr>
+						            <td>
+						                Prithviraj Billa
+						            </td>
+						            <td>
+						                <a href="mailto:prithviraj.m.billa@gmail.com">prithviraj.m.billa@gmail.com</a>
+						            </td>
+						            <td>
+						                9167428191
+						            </td>
+						        </tr>
+						        <tr>
+						            <td colspan="3">
+						                <h3>
+						                    <span style="font-size: 12pt;">
+						                        <strong>
+						                            Publi and Media Manager:
+						                        </strong>
+						                    </span>
+						                </h3>
+						            </td>
+						        </tr>
+						        <tr>
+						            <td>
+						                Pranav Chandra
+						            </td>
+						            <td>
+						                <a href="mailto:pranav.rockzzz007@gmail">pranav.rockzzz007@gmail</a>
+						            </td>
+						            <td>
+						                9167343956
+						            </td>
+						        </tr>
+						        <tr>
+						            <td>
+						                Anurag Mundhada
+						            </td>
+						            <td>
+						                anuragmundhada@gmail.com
+						            </td>
+						            <td>
+						                9769486724
+						            </td>
+						        </tr>
+						    </tbody>
+						</table>
+
+								
+														
+						</section>
+						
+					
+					</div><!-- ./col -->
+					
+						
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+			
+</main>
+@endsection
