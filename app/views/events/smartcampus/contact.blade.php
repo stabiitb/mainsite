8e8c908c5f9a17fa64bae7d5108ba761afc165ec
prateekchandan <prateekchandan5545@gmail.com>
2014-11-05 23:46:06 +0530

    updated contacts

--- app/views/events/smartcampus/contact.blade.php
+++ app/views/events/smartcampus/contact.blade.php
@@ -44,13 +44,30 @@
 					</div><!-- /.row -->
 
 					<div class="row  text-center">
-						<div class="col-sm-6  col-sm-offset-3 inner-left inner-right">
+						<div class="col-sm-6 inner-left inner-right">
 							<figure class="member">
-								
+								<div class="icn-link">
+									<span class="icn-more"></span><img src="https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-xfa1/v/t1.0-9/10356259_10201926760444212_7161558901692015539_n.jpg?oh=b5c3f8a327c1fb1d4189b08fbedabbf8&oe=54D775FB&__gda__=1424858390_8e6f9204e580f20fd5443401da16f42f" class="img-circle">
+								</div><!-- /.icon-overlay -->
 								<figcaption>
 									<h2>
 										Rythima Shinde
+										<span><i class="icon-phone"></i>7506134411</span>
 										<span><i class="icon-mail">rhythimashinde@gmail.com</i></span>
+									</h2>
+								</figcaption>	
+							</figure>
+						</div><!-- /.col -->
+						<div class="col-sm-6 inner-left inner-right">
+							<figure class="member">
+								<div class="icn-link">
+									<span class="icn-more"></span><img src="https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/10689952_764485593597222_564710123820242687_n.jpg?oh=ec9c19c9189d531cf09c376cbdf5b0bb&oe=54E77980&__gda__=1425094589_cd8fb79cdc0b3fcc39c6bb283193edec" class="img-circle">
+								</div><!-- /.icon-overlay -->
+								<figcaption>
+									<h2>
+										Madhuri Nagare <br>(PG Acad Council)
+										<span><i class="icon-phone"></i>9822662363</span>
+										<span><i class="icon-mail">madhurinagarepgacad@gmail.com</i></span>
 									</h2>
 								</figcaption>	
 							</figure>
