39e8a9ee2bb692ff15e10d85036be1adba8d6c9c
Siddharth Bulia <siddharth.bulia@gmail.com>
2015-03-25 13:45:22 +0530

    ITSP website

--- app/views/events/ITSP_2015/timeline.blade.php
+++ app/views/events/ITSP_2015/timeline.blade.php
@@ -1,5 +1,5 @@
 @extends('events.ITSP_2015.layout')
 @section('inner-content')
 Welcome to ITSP 2015
-<!--<iframe src='http://cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=0AolHa9BydZxLdHVoTXNjSjl2TmdkdGFBSDBvbkdiSFE&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650' width='100%' height='650' frameborder='0'></iframe>-->
+<iframe src='http://cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=0AolHa9BydZxLdHVoTXNjSjl2TmdkdGFBSDBvbkdiSFE&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650' width='100%' height='650' frameborder='0'></iframe>
 @endsection
