1079a205198fe5af8b22f70f86d240a6029ef669
prateekchandan <prateekchandan5545@gmail.com>
2015-03-25 04:12:34 -0400

    some changes in layout

--- app/views/events/ITSP_2015/timeline.blade.php
+++ app/views/events/ITSP_2015/timeline.blade.php
@@ -1,4 +1,5 @@
 @extends('events.ITSP_2015.layout')
 @section('inner-content')
-<iframe src='http://cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=0AolHa9BydZxLdHVoTXNjSjl2TmdkdGFBSDBvbkdiSFE&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650' width='100%' height='650' frameborder='0'></iframe>
+Welcome to ITSP 2015
+<!--<iframe src='http://cdn.knightlab.com/libs/timeline/latest/embed/index.html?source=0AolHa9BydZxLdHVoTXNjSjl2TmdkdGFBSDBvbkdiSFE&font=Bevan-PotanoSans&maptype=toner&lang=en&height=650' width='100%' height='650' frameborder='0'></iframe>-->
 @endsection
