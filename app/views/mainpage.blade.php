a7baab2e13b2f5b3986563920308de0a67bdc2c4
Siddharth Bulia <siddharth.bulia@gmail.com>
2015-03-25 13:46:51 +0530

    ITSP website

--- app/views/mainpage.blade.php
+++ app/views/mainpage.blade.php
@@ -14,11 +14,11 @@
 						<div class="col-sm-10 center-block text-center">
 							<ul>
 							<li class="single-block">ITSP 2015 &nbsp <a href="{{URL::route('events.ITSP_2015.form')}}" class="btn ">Register</a></li>
-							<li class="single-block">1) If your idea is selected, you need not worry about room retention, money, anything at all. <br>
+							<!-- <li class="single-block">1) If your idea is selected, you need not worry about room retention, money, anything at all. <br>
 													 2) You need not necessarily have a team while registering now.<br>
 													 3) We'll soon have multiple brainstorming sessions, do attend.<br>
 													 4) Abstract submission deadline will soon be announced.<br>
-													 For further queries join the <a href="https://www.facebook.com/groups/1566380240276628/"><b>ITSP FB group</b></a>.</li>
+													 For further queries join the <a href="https://www.facebook.com/groups/1566380240276628/"><b>ITSP FB group</b></a>.</li> -->
 							</ul>
 						</div><!-- /.col -->
 					</div><!-- /.row -->
