50b636accac59aa98c81ddf44f7b150f81d47675
Siddharth Bulia <siddharth.bulia@gmail.com>
2015-04-02 14:13:22 +0530

    Added itsp mentor link

--- app/views/events/ITSP_2015/mentor.blade.php
+++ app/views/events/ITSP_2015/mentor.blade.php
@@ -1,5 +1,5 @@
 
 @extends('events.ITSP_2015.layout')
 @section('inner-content')
-<iframe width="100%" height="600px" src="https://docs.google.com/forms/d/1_bc7fR5Yeyycg3hgqIzT7o6IKemGiyF3YjX0v5zEUF8/viewform?edit_requested=true"></iframe>
+<iframe width="100%" height="600px" src="https://docs.google.com/forms/d/1_bc7fR5Yeyycg3hgqIzT7o6IKemGiyF3YjX0v5zEUF8/viewform"></iframe>
 @endsection
