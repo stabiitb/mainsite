2d9fd8348e387d67d92f5e9e81e57f1e400d71d2
Siddharth Bulia <siddharth.bulia@gmail.com>
2015-04-03 13:55:28 +0530

    Added content in mentor form

--- app/views/events/ITSP_2015/mentor.blade.php
+++ app/views/events/ITSP_2015/mentor.blade.php
@@ -1,5 +1,6 @@
 
 @extends('events.ITSP_2015.layout')
 @section('inner-content')
+<h3>Login with Gmail to access the form.</h3>
 <iframe width="100%" height="600px" src="https://docs.google.com/forms/d/1_bc7fR5Yeyycg3hgqIzT7o6IKemGiyF3YjX0v5zEUF8/viewform"></iframe>
 @endsection
