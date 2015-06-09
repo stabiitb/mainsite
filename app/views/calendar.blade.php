6f8d4da8fe124c8d2d54f10f13b141a1a9d448b1
Siddharth <siddharth.bulia@gmail.com>
2015-06-09 13:05:24 +0530

    added calender

--- app/views/calendar.blade.php
+++ app/views/calendar.blade.php
@@ -4,7 +4,14 @@
 <main>
 			
 			<section>
-<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showTabs=0&amp;src=siddharth.bulia%40gmail.com&ctz=Asia/Calcutta&amp;src=fvktl04jnjac2ns9hh080fol2g%40group.calendar.google.com&ctz=Europe/Vienna&amp;src=darshildave94%40gmail.com&ctz=Asia/Calcutta&amp;src=yrajsm%40gmail.com&ctz=Asia/Calcutta&amp;src=rushilmodi28%40gmail.com&ctz=Asia/Calcutta" style="border: 0" width="800" height="600" frameborder="0" scrolling="no">
+<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showTabs=0&amp;
+src=siddharth.bulia%40gmail.com&amp;ctz=Asia/Calcutta&amp;color=%23FF00FF
+&amp;src=fvktl04jnjac2ns9hh080fol2g%40group.calendar.google.com&amp;ctz=Europe/Vienna&amp;color=%23FFFF00
+&amp;src=darshildave94%40gmail.com&amp;ctz=Asia/Calcutta&amp;color=%23FF0000
+&amp;src=yrajsm%40gmail.com&amp;ctz=Asia/Calcutta&amp;color=%23FF9900
+&amp;src=rushilmodi28%40gmail.com&amp;ctz=Asia/Calcutta&amp;color=%23FFFF00
+"
+ style="border: 0;color=red" width="1000" height="600" frameborder="0" scrolling="no">
 </section>				
 </main>
 @endsection
