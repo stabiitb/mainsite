befa1f43f8306efcffa258776d20bbdba5e81864
Siddharth <siddharth.bulia@gmail.com>
2015-06-11 04:10:17 +0530

    Removed Siddharth calender from Stab Calender

--- app/views/calendar.blade.php
+++ app/views/calendar.blade.php
@@ -5,8 +5,7 @@
 			
 			<section>
 <iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showTabs=0&amp;
-src=siddharth.bulia%40gmail.com&amp;ctz=Asia/Calcutta&amp;color=%23FF00FF
-&amp;src=fvktl04jnjac2ns9hh080fol2g%40group.calendar.google.com&amp;ctz=Europe/Vienna&amp;color=%23FFFF00
+&amp;src=fvktl04jnjac2ns9hh080fol2g%40group.calendar.google.com&amp;ctz=Asia/Calcutta&amp;color=%23FFFF00
 &amp;src=darshildave94%40gmail.com&amp;ctz=Asia/Calcutta&amp;color=%23FF0000
 &amp;src=yrajsm%40gmail.com&amp;ctz=Asia/Calcutta&amp;color=%23FF9900
 &amp;src=rushilmodi28%40gmail.com&amp;ctz=Asia/Calcutta&amp;color=%23FFFF00
