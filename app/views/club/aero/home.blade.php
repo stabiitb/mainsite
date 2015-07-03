f7df58ad4f574951b8d0412b234c0cb733f3b3a4
shubham-goel <sgoel160@gmail.com>
2015-07-03 23:05:28 +0530

    modified Aero Page

--- app/views/club/aero/home.blade.php
+++ app/views/club/aero/home.blade.php
@@ -6,6 +6,8 @@
 </figure>
 <h1>AEROMODELLING CLUB</h1>
 <p>
-	Ever wanted to pilot your own plane . The biggest takeaway from 3 Idiots for you was a desire to build a quadrotor . Have a passion for modelling . If true then the Aeromodelling Club is the place for you . Essentially the Aeromodelling Club is a group for aviation and aeromodelling enthusiasts in the institute. The activities of the club include RC Plane making , lectures and talks on various aeromodelling and aviation topics and simulator flying. Lectures and discussions are held every fortnight and are open to all. The club also supports the freshers in making their first model airplanes. The google group for the club is a site for active discussion on project ideas and topics related to flight.Over the years many projects have been undertaken including hovercrafts , and an underwater glider .. Come join us on our journey to understand the secrets of flight.
+	Ever wondered what makes stuff airborne? Ever harboured a dream to sail to the tune of the winds? Aeromodelling is a science for some, a sport for many...but for us it is a hobby. We, at Aeromodelling Club, IIT Bombay are group of aeromodelling and aviation enthusiasts sharing a unique passion of flying and an innate desire to push the bounds of airspace like they have never been before.
+	<br><br>
+	Come be a part of us...come celebrate AVIATION!!!
 </p>		
 @endsection
