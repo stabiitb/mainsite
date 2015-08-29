8c465fe73643444a491c31d674be55e31adb54ac
Kushal-IITB <babel97kushal@gmail.com>
2015-08-29 20:08:44 +0530

    RC_Plane_Resource

--- app/views/club/aero/RC_Plane/resources.blade.php
+++ app/views/club/aero/RC_Plane/resources.blade.php
@@ -3,9 +3,15 @@
 						<section id="reg" >
 							<div class="row">			
 								<div class="col-sm-12 inner-right-xs inner-bottom-sm">
-									<h2>Stay Tuned :)</h2>
+									<h2>References to help you excel :)</h2>
 								</div><!-- /.col -->
 							</div>
-							
+							<div class="row">
+								
+									<a target = "blank" href = {{ URL::asset('media/2015/club_assets/aero/RC_Plane_Basics .pptx')}}><h3><i>RC Plane Basics</i></h3></a>
+									<br>
+									
+								
+							</div><!-- ./row -->
 						</section>
 @endsection
