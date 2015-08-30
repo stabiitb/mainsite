2a22ced59638ea569e1e041a39ccdd382bf5a848
shubham-goel <sgoel160@gmail.com>
2015-08-30 19:15:46 +0530

    FB pluigins for all clubs added, Added web team members to Team Page

--- app/views/club/aero/layout.blade.php
+++ app/views/club/aero/layout.blade.php
@@ -6,7 +6,14 @@
 		<!-- ============================================================= MAIN ============================================================= -->
 		
 		<main>
-			
+			<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+			  var js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.4";
+			  fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));</script>
 			<div class="container inner">
 				<div class="row">
 					
@@ -23,6 +30,8 @@
 							<li><a href="{{URL::Route('aeromodelling-club')}}/gallery">Gallery</a></li>
 							<!--<li><a href="{{URL::Route('aeromodelling-club')}}/video">Videos</a></li>-->
 						</ul><!-- /.sidenav -->
+						<br>
+						<div class="fb-page" data-href="https://www.facebook.com/pages/Aeromodelling-Club-IIT-Bombay/261970783815703?fref=ts" data-tabs="timeline" data-width="350" data-height="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/pages/Aeromodelling-Club-IIT-Bombay/261970783815703?fref=ts"><a href="https://www.facebook.com/pages/Aeromodelling-Club-IIT-Bombay/261970783815703?fref=ts">Aeromodelling Club IIT Bombay</a></blockquote></div></div>
 						
 						<!-- ============================================================= SIDE NAVIGATION : END ============================================================= -->
 						
