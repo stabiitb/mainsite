280ba7ce28903cc84e939e9f1ee9f64f07776f07
Siddharth Bulia <siddharth.bulia@gmail.com>
2015-09-15 19:24:32 +1000

    Added RC Plane Presentation

--- app/views/club/aero/RC_Plane/resources.blade.php
+++ app/views/club/aero/RC_Plane/resources.blade.php
@@ -8,7 +8,7 @@
 							</div>
 							<div class="row">
 								
-									<a target = "blank" href = {{ URL::asset('media/2015/club_assets/aero/RC_Plane_Basics.pptx')}}><h3><i>RC Plane Basics</i></h3></a>
+									<a target = "blank" href = {{ URL::asset('/media/2015/club_assets/aero/Events/RC_Plane/Resources/RC_Plane_Basics.pptx')}}><h3><i>RC Plane Basics</i></h3></a>
 									<br>
 									<a target = "blank" href = "https://play.google.com/store/apps/details?id=leofs.android.free&feature=search_result#?t=W251bGwsMSwxLDEsImxlb2ZzLmFuZHJvaWQuZnJlZSJd"><h3><i>Leo's RC Simulator App link</i></h3></a>
 									<br>
