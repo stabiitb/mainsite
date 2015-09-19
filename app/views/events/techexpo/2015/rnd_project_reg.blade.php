6c8e9fdd6b15c977c1bfc7d041b75f9c97df333a
Siddharth Bulia <siddharth.bulia@gmail.com>
2015-09-19 18:02:25 +1000

    Added link of rnd expo to mainpage

--- app/views/events/techexpo/2015/rnd_project_reg.blade.php
+++ app/views/events/techexpo/2015/rnd_project_reg.blade.php
@@ -4,6 +4,8 @@
 							<div class="row">			
 								<div class="col-sm-12 inner-right-xs inner-bottom-sm">
 									<h2>Tech & RnD Exposition 2015 - RnD Projects Registration</h2>
+									<p> This form is only for RnD or SURP(formerly ISPA) or some research projects you did as a part of your internship.</p>
+									<p> For registering your ITSP projects/self tech project, please register <u><a href="{{URL::Route('events.techexpo.2015')}}/registration_tech_projects">here</a></u></p>  
 								</div><!-- /.col -->
 							</div>
 							<div class="row">
--- app/views/events/techexpo/2015/tech_project_reg.blade.php
+++ app/views/events/techexpo/2015/tech_project_reg.blade.php
@@ -4,6 +4,8 @@
 							<div class="row">			
 								<div class="col-sm-12 inner-right-xs inner-bottom-sm">
 									<h2>Tech & RnD Exposition 2015 - Technical Projects Registration</h2>
+									<p> This form is only for ITSP projects/tech projects.</p>
+									<p> For registering your Rnd projects, please register <u><a href="{{URL::Route('events.techexpo.2015')}}/registration_rnd_projects">here</a></u></p>  
 								</div><!-- /.col -->
 							</div>
 							<div class="row">
