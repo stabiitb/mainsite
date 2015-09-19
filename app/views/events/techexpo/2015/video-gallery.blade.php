d4bb64980720a4d0a564b79e7bd6fb95804efcc2
Siddharth Bulia <siddharth.bulia@gmail.com>
2015-09-19 18:08:52 +1000

    Added video to video page

--- app/views/events/techexpo/2015/video-gallery.blade.php
+++ app/views/events/techexpo/2015/video-gallery.blade.php
@@ -5,6 +5,17 @@
 								<div class="col-sm-12 inner-right-xs inner-bottom-sm">
 									<h2>Glimpse of Tech & RnD Exposition 2014</h2>
 								</div><!-- /.col -->
+							</div>
+							<div class="row">
+								<div class="col-md-2"></div>
+								<div class="col-md-8">
+									<iframe width="560" height="315" src="https://www.youtube.com/embed/8xyVpbR3DJc" frameborder="0" allowfullscreen></iframe>
+									<h3 class="text-center"><u>Tech & RnD Exposition 2015 is coming !! </u> </h3>		
+								</div>
+								<div class="col-md-2"></div>
+							</div>
+							<br>
+							<div class="row">
 								<div class="col-md-2"></div>
 								<div class="col-md-8">
 										<iframe width="560" height="315" src="https://www.youtube.com/embed/H5pS1f3evbU" frameborder="0" allowfullscreen></iframe>								
