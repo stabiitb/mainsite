bc5c007f8a0d07afe211c5d553bcee101f9ba8d3
siddharth bulia <siddharth.bulia@gmail.com>
2016-02-11 09:45:10 +0530

    adding por page

--- app/views/events/PoR/form.blade.php
+++ app/views/events/PoR/form.blade.php
@@ -11,7 +11,7 @@
 	}
 </style>
 <main>
-<iframe id="typeform-full" width="100%" height="100%" frameborder="0" src="http://goo.gl/forms/KBGXpNSN3K"></iframe>
+<iframe id="typeform-full" width="100%" height="100%" frameborder="0" src="https://docs.google.com/forms/d/1f6q-G9FHY6svlvdi3IuBDjH3T2Hyq8nV8AqpWie0Ra0/viewform?c=0&w=1"></iframe>
 <!-- <script type="text/javascript" src="https://s3-eu-west-1.amazonaws.com/share.typeform.com/embed.js"></script> -->
 </main>
 @else
