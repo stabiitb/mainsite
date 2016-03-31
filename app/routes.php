7763943756b7ac413dcae4b39ee738c59733dc1a
ARKA <ark.sadhu2904@gmail.com>
2016-04-01 01:35:02 +0530

    removing technical coordinators form

--- app/routes.php
+++ app/routes.php
@@ -98,7 +98,7 @@ Route::group(array('prefix'=>'positionofresponsibility'),function()
 {
     Route::get('/', array('as'=>'events.PoR.form','uses'=>'PoRController@form'));
     Route::get('register',array('as'=>'events.PoR.form','uses'=>'PoRController@form'));
-    Route::get('techcoordinators2k16',array('as'=>'events.PoR.form', 'uses'=>'PoRController@form'));
+//    Route::get('techcoordinators2k16',array('as'=>'events.PoR.form', 'uses'=>'PoRController@form'));
 });
     
 
