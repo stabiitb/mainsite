577cfacf71339b55079884d0971ed530394f0a38
ARKA <ark.sadhu2904@gmail.com>
2016-04-04 02:56:48 +0530

    updated team 2016 page

--- app/views/team/2016.blade.php
+++ app/views/team/2016.blade.php
@@ -8,10 +8,456 @@
 						<div class="col-md-8 col-sm-10 center-block text-center " >
 							<header>
 								<h1>Team STAB 2016</h1>
-								<h3>To be updated soon</h3>
+								{{--<h3>Technical Coordinators To be updated soon</h3>--}}
 							</header>
 						</div><!-- /.col -->
 					</div><!-- /.row -->
                 </div>
+
+	<div class="row  text-center">
+		<div class="col-sm-6 inner-left inner-right" >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/atish.jpg')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+
+				<figcaption>
+					<h2>
+						Atish Aloor
+						<span>Overall Coordinator</span>
+						<span><i class="icon-phone">9987700750</i></span>
+						<span><i class="icon-mail">aloor.atish@gmail.com</i></span>
+					</h2>
+				</figcaption>
+			</figure>
+		</div><!-- /.col -->
+        <div class="col-sm-6 inner-left inner-right" >
+            <figure class="member">
+                <div class="icn-link">
+                    <span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/vaibhav.jpg')}}" class="img-circle">
+
+                </div><!-- /.icon-overlay -->
+
+                <figcaption>
+
+                    <h2>
+                        Vaibhav Bhosale
+                        <span>UG Nominee</span>
+                        <span><i class="icon-phone">940596175</i></span>
+                        <span><i class="icon-mail"> vaibhavbhosale15@gmail.com</i></span>
+                    </h2>
+                </figcaption>
+
+            </figure>
+        </div><!-- /.col -->
+	</div><!-- /.row -->
+    <hr>
+    <div class="row  text-center">
+        <div class="col-sm-6 col-sm-offset-3 inner-left inner-right " >
+            <figure class="member">
+                <div class="icn-link">
+                    <span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Swapnil.jpg')}}" class="img-circle">
+                </div><!-- /.icon-overlay -->
+
+                <figcaption>
+                    <h2>
+                        Swapnil Ransing
+                        <span>Institute Secretary for Student Reading Groups</span>
+                        <span><i class="icon-phone">9665770449</i></span>
+                        <span><i class="icon-mail">swapnilbransing@gmail.com</i></span>
+                    </h2>
+                </figcaption>
+
+            </figure>
+        </div><!-- /.col -->
+    </div><!-- /.row -->
+	<hr>
+	<div class="row">
+		<div class="col-md-8 col-sm-10 center-block text-center " >
+			<header>
+				<h1>Club Managers</h1>
+			</header>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+	<hr>
+	<div class="row">
+		<div class="col-md-8 col-sm-10 center-block text-center " >
+			<header>
+				<h3>Aeromodelling Club</h3>
+			</header>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+
+	<div class="row  text-center">
+		<div class="col-sm-6 col-sm-offset-3 inner-left inner-right " >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Yogesh_Kumar.jpg')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+
+				<figcaption>
+					<h2>
+						Yogesh Kumar
+						<span><i class="icon-phone">9619087152</i></span>
+						<span><i class="icon-mail">02yogesh16@gmail.com</i></span>
+					</h2>
+				</figcaption>
+
+			</figure>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+	<hr>
+	<div class="row">
+		<div class="col-md-8 col-sm-10 center-block text-center " >
+			<header>
+				<h3>Bio Tech Club</h3>
+			</header>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+
+	<div class="row  text-center">
+		<div class="col-sm-6 inner-left inner-right " >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Varunesh Goyal')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+
+				<figcaption>
+					<h2>
+						Varunesh Goyal
+						<span><i class="icon-phone">9004531127</i></span>
+						<span><i class="icon-mail">varuneshgoyal@gmail.com</i></span>
+					</h2>
+				</figcaption>
+
+			</figure>
+		</div><!-- /.col -->
+		<div class="col-sm-6 inner-left inner-right " >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Siddhant')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+				<figcaption>
+					<h2>
+						Siddhant Garg
+						<span><i class="icon-phone">9820967954</i></span>
+						<span><i class="icon-mail">sid7954@gmail.com</i></span>
+					</h2>
+				</figcaption>
+			</figure>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+
+	<hr>
+	<div class="row">
+		<div class="col-md-8 col-sm-10 center-block text-center " >
+			<header>
+				<h3>Electronics Club:</h3>
+			</header>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+
+	<div class="row  text-center">
+		<div class="col-sm-6 inner-left inner-right " >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Meet_Udeshi')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+
+				<figcaption>
+					<h2>
+						Meet Udeshi
+						<span><i class="icon-phone">9619221240</i></span>
+						<span><i class="icon-mail">mudeshi1209@gmail.com</i></span>
+					</h2>
+				</figcaption>
+
+			</figure>
+		</div><!-- /.col -->
+		<div class="col-sm-6 inner-left inner-right " >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Parth Kothari')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+				<figcaption>
+					<h2>
+						Parth Kothari
+						<span><i class="icon-phone">9167633979</i></span>
+						<span><i class="icon-mail">parthkothari811@gmail.com</i></span>
+					</h2>
+				</figcaption>
+			</figure>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+	<hr>
+	<div class="row">
+		<div class="col-md-8 col-sm-10 center-block text-center " >
+			<header>
+				<h3>Krittika (Astronomy Club)</h3>
+			</header>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+
+	<div class="row  text-center">
+		<div class="col-sm-6 col-sm-offset-3 inner-left inner-right " >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Nilaksh Aggarwal.jpg')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+
+				<figcaption>
+					<h2>
+                        Nilaksh Kumar Aggarwal
+						<span><i class="icon-phone">8828291216</i></span>
+						<span><i class="icon-mail">nilakshkaggarwal@gmail.com</i></span>
+					</h2>
+				</figcaption>
+
+			</figure>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+
+	<hr>
+	<div class="row">
+		<div class="col-md-8 col-sm-10 center-block text-center " >
+			<header>
+				<h3>Maths &amp; Physics Club</h3>
+			</header>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+
+	<div class="row  text-center">
+		<div class="col-sm-6 inner-left inner-right " >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Aditi Sharma')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+
+				<figcaption>
+					<h2>
+						Aditi Sharma
+						<span><i class="icon-phone">    9987683851   </i></span>
+						<span><i class="icon-mail">aditis0997@gmail.com</i></span>
+					</h2>
+				</figcaption>
+
+			</figure>
+		</div><!-- /.col -->
+		<div class="col-sm-6 inner-left inner-right " >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Sandesh Kalantre')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+
+				<figcaption>
+					<h2>
+						Sandesh Kalantre
+						<span><i class="icon-phone">    9881130761   </i></span>
+						<span><i class="icon-mail">  kalantresandesh@gmail.com   </i></span>
+					</h2>
+				</figcaption>
+
+			</figure>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+	<hr>
+	<div class="row">
+		<div class="col-md-8 col-sm-10 center-block text-center " >
+			<header>
+				<h3>Projects Manager</h3>
+			</header>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+
+	<div class="row  text-center">
+		<div class="col-sm-6 inner-left inner-right " >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Shalaka Kulkarni')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+
+				<figcaption>
+					<h2>
+						Shalaka Kulkarni
+						<span><i class="icon-phone">7506438504</i></span>
+						<span><i class="icon-mail">k.shalaka96@gmail.com</i></span>
+					</h2>
+				</figcaption>
+
+			</figure>
+		</div><!-- /.col -->
+		<div class="col-sm-6 inner-left inner-right " >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Sudeep salgia')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+				<figcaption>
+					<h2>
+						Sudeep Salgia
+						<span><i class="icon-phone">9987894735</i></span>
+						<span><i class="icon-mail">suda96salgia@gmail.com</i></span>
+					</h2>
+				</figcaption>
+			</figure>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+	<hr>
+	<div class="row">
+		<div class="col-md-8 col-sm-10 center-block text-center " >
+			<header>
+				<h3>Robotics Club</h3>
+			</header>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+
+	<div class="row  text-center">
+		<div class="col-sm-6 inner-left inner-right " >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Sravan Patchala')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+
+				<figcaption>
+					<h2>
+						Sravan Patchala
+						<span><i class="icon-phone">8976087774</i></span>
+						<span><i class="icon-mail">sravps7@gmail.com</i></span>
+					</h2>
+				</figcaption>
+
+			</figure>
+		</div><!-- /.col -->
+		<div class="col-sm-6 inner-left inner-right " >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Soham Shanbagh.jpg')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+
+				<figcaption>
+					<h2>
+						Soham Shanbhag
+						<span><i class="icon-phone">9869005901</i></span>
+						<span><i class="icon-mail">soham.shanbhag@gmail.com</i></span>
+					</h2>
+				</figcaption>
+
+			</figure>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+	<hr>
+	<div class="row">
+		<div class="col-md-8 col-sm-10 center-block text-center " >
+			<header>
+				<h3>Web and Coding Club</h3>
+			</header>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+
+	<div class="row  text-center">
+		<div class="col-sm-6 inner-left inner-right " >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Kalpesh Krishna')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+
+				<figcaption>
+					<h2>
+						Kalpesh Krishna
+						<span><i class="icon-phone">9822650831</i></span>
+						<span><i class="icon-mail">kalpeshk2011@gmail.com</i></span>
+					</h2>
+				</figcaption>
+
+			</figure>
+		</div><!-- /.col -->
+		<div class="col-sm-6 inner-left inner-right " >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Cheeku')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+				<figcaption>
+					<h2>
+						Kumar Ayush
+						<span><i class="icon-phone">9468854691</i></span>
+						<span><i class="icon-mail">cheekujodhpur@gmail.com</i></span>
+					</h2>
+				</figcaption>
+			</figure>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+
+
+    <hr>
+    <div class="row">
+        <div class="col-md-8 col-sm-10 center-block text-center " >
+            <header>
+                <h3>Tinkerers' Lab</h3>
+            </header>
+        </div><!-- /.col -->
+    </div><!-- /.row -->
+
+    <div class="row  text-center">
+        <div class="col-sm-6 inner-left inner-right " >
+            <figure class="member">
+                <div class="icn-link">
+                    <span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Naman Gupta.jpg')}}" class="img-circle">
+                </div><!-- /.icon-overlay -->
+
+                <figcaption>
+                    <h2>
+                        Naman Gupta
+                        <span>Tinkerers' Lab and Infrastructure</span>
+                        <span><i class="icon-phone">8989409444</i></span>
+                        <span><i class="icon-mail">namang.iitb@gmail.com</i></span>
+                    </h2>
+                </figcaption>
+
+            </figure>
+        </div><!-- /.col -->
+        <div class="col-sm-6 inner-left inner-right " >
+            <figure class="member">
+                <div class="icn-link">
+                    <span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Rakshit Jain.jpg')}}" class="img-circle">
+                </div><!-- /.icon-overlay -->
+                <figcaption>
+                    <h2>
+                        Rakshit Jain
+                        <span>Tinkerers' Lab Public and Alumni Relations </span>
+                        <span><i class="icon-phone">8828294852</i></span>
+                        <span><i class="icon-mail">rakshit28081996@gmail.com</i></span>
+                    </h2>
+                </figcaption>
+            </figure>
+        </div><!-- /.col -->
+    </div><!-- /.row -->
+
+	<hr>
+	<div class="row" id="Webteam">
+		<div class="col-md-8 col-sm-10 center-block text-center " >
+			<header>
+				<h3>Web Manager</h3>
+			</header>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+
+	<div class="row  text-center">
+		<div class="col-sm-6 col-sm-offset-3 inner-left inner-right " >
+			<figure class="member">
+				<div class="icn-link">
+					<span class="icn-more"></span><img src="{{URL::asset('assets/images/2016/Arka Sadhu')}}" class="img-circle">
+				</div><!-- /.icon-overlay -->
+				<figcaption>
+					<h2>
+						Arka Sadhu
+						<span><i class="icon-phone">9867331197</i></span>
+						<span><i class="icon-mail">blackreaper4039@gmail.com</i></span>
+					</h2>
+				</figcaption>
+			</figure>
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+
+
+
 </section>
 @endsection
