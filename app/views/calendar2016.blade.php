4b920eff9500f06ad0bb75c8846cad905837a61d
Arka Sadhu <ark.sadhu2904@gmail.com>
2016-06-07 12:31:33 +0300

    updating title for calendar2016

--- app/views/calendar2016.blade.php
+++ app/views/calendar2016.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <main>
         <section>
-          <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=a3rr86qgippgh8ua80e508l4vg%40group.calendar.google.com&amp;color=%23691426&amp;src=i4ub9n4vdp70c35820lfhtagvc%40group.calendar.google.com&amp;color=%238C500B&amp;src=9h5p0ki7gnmair697q3od6o2ag%40group.calendar.google.com&amp;color=%235229A3&amp;src=oeptbo1ms672rjuv1pbl023hqk%40group.calendar.google.com&amp;color=%232F6309&amp;src=eds69f70uu4c46ackcugkei7hc%40group.calendar.google.com&amp;color=%238D6F47&amp;ctz=Asia%2FCalcutta" style="border-width:0" width="1200" height="600" frameborder="0" scrolling="no"></iframe>
-        </section>
+            <iframe src="https://calendar.google.com/calendar/embed?title=Stab%20Calendar%202016-17&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=a3rr86qgippgh8ua80e508l4vg%40group.calendar.google.com&amp;color=%23691426&amp;src=i4ub9n4vdp70c35820lfhtagvc%40group.calendar.google.com&amp;color=%238C500B&amp;src=9h5p0ki7gnmair697q3od6o2ag%40group.calendar.google.com&amp;color=%235229A3&amp;src=oeptbo1ms672rjuv1pbl023hqk%40group.calendar.google.com&amp;color=%232F6309&amp;src=eds69f70uu4c46ackcugkei7hc%40group.calendar.google.com&amp;color=%238D6F47&amp;ctz=Asia%2FCalcutta" style="border-width:0" width="1200" height="600" frameborder="0" scrolling="no"></iframe>        </section>
     </main>
 @endsection
