f3bd7e6a5f1c43bd1b18255b28797b15c15432e4
Shounak710 <shounak647@gmail.com>
2017-04-03 18:59:38 +0530

    ITSP mentor form

--- app/views/events/ITSP_2016/mentor.blade.php
+++ app/views/events/ITSP_2016/mentor.blade.php
@@ -1,5 +1,5 @@
 
 @extends('events.ITSP_2016.layout')
 @section('inner-content')
-    <iframe src="https://docs.google.com/forms/d/1K6wAt_2CLo3BbIqaQpxApbOHFp3vA3ld47BN4RrYhv0/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdqCo5zUumGWwD-a3rKgNsauxbIiqh-4uboHXU2XqO65G43MA/viewform?usp=sf_link" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 @endsection
